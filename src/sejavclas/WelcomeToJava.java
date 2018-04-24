64c1ba515a9f82df08d5572c1c409a0248bcc561
Petrina <Petrina@ED2015>
2018-04-24 18:50:59 -0500

    Creating first repository

---
+++ src/sejavclas/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package sejavclas;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Here Is What's Being Printed");
+	}
+
+}
